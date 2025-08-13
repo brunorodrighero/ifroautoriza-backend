8db2787a2435f120decf9b76b81e6078d63ccac6
Bruno Rodighiero <bruno.rodrighero@gmail.com>
2025-08-13 15:15:23 -0400

    fix(api): Corrige import ausente de datetime no health check

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import time
+from datetime import datetime
 
 from src.core.config import settings
 from src.utils.logger import logger
